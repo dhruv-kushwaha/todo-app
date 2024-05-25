1991f7473854dcbe22fc18d6224a0ddb1cae5aed
Dhruv Kushwaha <101521624+dhruv-kushwaha@users.noreply.github.com>
2024-05-25 19:21:38 +0530

    Update global.ts

--- frontend/src/services/global.ts
+++ frontend/src/services/global.ts
@@ -1,4 +1,5 @@
+// Deployed Render Backend URL
 export const URL = `https://todo-app-y8dw.onrender.com/api/v1`;
 
-// Local Backend
+// Local Backend URL
 // export const URL = `http://localhost:3030/api/v1`;
